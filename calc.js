83a28fc9bb35b9ad1cf2a29fe5626bd1fd57c59b
NikolasUlas <Nikolas@iMac-Nikolaj.local>
2016-06-04 19:56:37 +0300

    bugfix #1

--- calc.js
+++ calc.js
@@ -2,56 +2,60 @@
 
 class Calc {
     constructor() {
-    this._screen = document.querySelector('.screen');
-    this._screenValue = this._screen.innerHTML;
-    this._buttons = document.querySelectorAll('.buttons');
+
+        this._screen = document.querySelector('.screen');
+        this._buttons = document.querySelectorAll('.buttons');
+
+        for (var i = 0; i < this._buttons.length; i++) {
+            this._buttons[i].onclick = this._onBtnClick.bind(this);
+        }
         
-    for(var i =0; i < this._buttons.length; i++) { this._buttons[i].onclick = this._onBtnClick.bind(this); }
-        
-    }
-    
-    _onBtnClick(event) {
-        
-       switch(event.currentTarget.innerHTML) {
-            case '0' : this._screenValue += 0;
-               break;
-            case '1' : this._screenValue += 1;
-               break;
-            case '2' : this._screenValue += 2;
-               break;
-            case '3' : this._screenValue += 3;
-               break;
-            case '4' : this._screenValue += 3;
-               break;
-            case '5' : this._screenValue += 3;
-               break;
-            case '6' : this._screenValue += 3;
-               break;
-            case '7' : this._screenValue += 3;
-               break;
-            case '8' : this._screenValue += 3;
-               break;
-            case '9' : this._screenValue += 3;
-               break;
-            case '÷' : this._screenValue += '/';
-               break;
-            case 'x' : this._screenValue += '*';
-               break;
-            case 'c' : this._screenValue = '';
-               break;
-            case '+' : this._screenValue += '+';
-               break;
-            case '-' : this._screenValue += '-';
-               break;
-            case '=' : this._screenValue = eval(this._screenValue);
-               break;
-            case '.' : this._screenValue += '.';
-               break;
-            default : console.log(this._screenValue);
-       }
+        this.onkeydown = this._onKeyboardClick.bind(this);
 
     }
-    
+
+
+    _onBtnClick(event) {
+
+        var button = event.currentTarget.innerHTML;
+        console.log(button);
+        var operators = ['+', '-', 'x', '÷', '.', '+'];
+        var lastChar = this._screen.innerHTML[this._screen.innerHTML.length - 1];
+
+        if (button == 'c') {
+            this._screen.innerHTML = '';
+            return true;
+        }
+
+        if (button == '=') {
+            let result = this._screen.innerHTML;
+            result = result.replace(/x/g, '*').replace(/÷/g, '/');
+            this._screen.innerHTML = eval(result);
+            return true;
+        }
+
+        if (button == '+/-') {
+            if (this._screen.innerHTML = ' ') {
+                this._screen.innerHTML += '-';
+            }
+
+            return true;
+        }
+
+        if (!isNaN(button)) {
+            this._screen.innerHTML += button;
+            return true;
+        }
+
+        if (operators.indexOf(button) || button == '+') {
+            if (!isNaN(lastChar)) {
+                this._screen.innerHTML += button;
+            }
+            
+            return true;
+        }
+    }
+     
 }
-    
+
 var app = new Calc;
